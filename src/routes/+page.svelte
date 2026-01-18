b5744a60e0c00f2c43ec6689ba9bff2b4f2b271e
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2026-01-18 16:22:30 +0530

    Added Simple Animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
+<script lang="ts">
+    
+//because i intend to use tauri
+import { onMount } from "svelte";
+import { gsap } from "gsap";
+
+let referece_to_text;
+
+onMount( ()=> gsap.from(referece_to_text, {x: 1000, duration: 2}) );
+</script>
 <svelte:head>
     <title>My SvelteKit App</title>
 </svelte:head>
--- src/routes/+page.ts
+++
@@ -1,8 +0,0 @@
-
-//because i intend to use tauri
-import { onMount } from "svelte";
-import { gsap } from "gsap";
-
-let referece_to_text;
-
-onMount( ()=> gsap.from(referece_to_text, {x: 1000, duration: 2}) );
